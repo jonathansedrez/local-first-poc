110570882d5d6c95141b701351d3e650d4df9128
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-05-21 22:05:49 -0300

    feat: add pglite integration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { db, initDb } from "./db.ts";
 
+// export window to DB to enable debug at browser console
 (window as any).db = db;
 
 initDb().then(() => {
